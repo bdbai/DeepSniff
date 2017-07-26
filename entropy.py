0e8f0d02056519413c6de375669168a86ec9a26d
yvbbrjdr <xyyqzy@hotmail.com>
2017-07-26 23:31:35 +0800

    calculate entropy of data

---
+++ entropy.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from math import log
+
+def entropy(data):
+    if isinstance(data, str):
+        data = data.encode()
+    if not isinstance(data, bytes):
+        raise Exception('entropy: entropy: error: data needs to be either a str or a bytes.')
+    ret = 0.0
+    length = len(data)
+    byteCount = [0] * (1 << 8)
+    for byte in data:
+        byteCount[byte] += 1
+    for count in byteCount:
+        if count > 0:
+            p = float(count) / length
+            ret -= p * log(p, 256)
+    return ret
+
+def test():
+    while True:
+        data = input('Input a string to calculate its entropy: ')
+        if len(data) > 0:
+            print(entropy(data))
+        else:
+            break
+
+if __name__ == '__main__':
+    test()
